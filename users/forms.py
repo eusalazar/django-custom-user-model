da5ee4717227ba002ddbd9025ef552ba09b15ef7
eusalazar <eugeniasalazar745@gmail.com>
2024-03-19 22:24:23 -0300

    latest-changes

--- users/forms.py
+++ users/forms.py
@@ -7,11 +7,10 @@ class CustomUserCreationForm(UserCreationForm):
 
   class Meta(UserCreationForm.Meta):
     model = CustomUser
-    fields = UserCreationForm.Meta.fields + ('age',)
-
+    fields = ('username', 'email', 'age',) # new
 
 class CustomUserChangeForm(UserChangeForm):
 
   class Meta:
     model = CustomUser
-    fields = UserChangeForm.Meta.fields
+    fields = ('username', 'email', 'age',) # new
